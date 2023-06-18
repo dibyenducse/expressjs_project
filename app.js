b86b43b7801b140370c81b8a7f0b922bc584f9c1
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-06-18 18:21:19 +0600

    application object done

--- app.js
+++ app.js
@@ -2,8 +2,34 @@ const express = require('express'); //express return function
 
 const app = express(); // app is a object
 //app.use(express.json()); //if client send Application/json data then this funtion can parse the data return object from req.body
-//app.use(express.raw()); // to rcv as buffer
-app.use(express.text()); // to rcv as buffer
+//app.use(express.raw()); // to rcv as buffer(if add toString() for getting string format)
+//app.use(express.text()); // to rcv as text
+//app.use(express.urlencoded()); // tp rcv encoded data
+
+// app.use(
+//     express.static(`${__dirname} + /public/`, {
+//         index: 'home.html',
+//     })
+// ); //to make the folder direct accesible by url
+
+app.set('view engine', 'ejs');
+
+app.use(
+    express.static(__dirname + '/public/', {
+        index: 'home.html',
+    })
+); //to make the folder direct accesible by url
+
+app.route('/about/mission') //for combinning single url with different method
+    .get((req, res) => {
+        res.send('welcome to application home get');
+    })
+    .put((req, res) => {
+        res.send('welcome to application home put');
+    })
+    .post((req, res) => {
+        res.send('welcome to application home post');
+    });
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
